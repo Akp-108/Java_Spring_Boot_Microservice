8f817d02e3c2855e287f53bfb3e23b57bf84fbff
atul <akp1081@gmail.com>
2024-06-01 01:09:20 +0530

    add plugin in pom file

---
+++ src/test/java/com/example/SpringHelloWorld/HelloControllerTest.java
@@ -0,0 +1,44 @@
+// package com.example.SpringHelloWorld;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class SpringHelloWorldApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
+
+
+package com.example.SpringHelloWorld;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("HELLO WORLD FROM HRS GROUP")));
+	}
+}
